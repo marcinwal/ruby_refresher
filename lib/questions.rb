6d477c7ce6803967f111226532c195f5b9e291e4
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-07 18:32:26 +0000

    2nd solver with regex

--- lib/questions.rb
+++ lib/questions.rb
@@ -5,6 +5,7 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.select { |el|  /^[aeiouy]/.match(el)}
 end
 
 # remove instances of nil (but NOT false) from an array
