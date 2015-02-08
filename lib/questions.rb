4953651ab1ccebeb52565c84ead627bf2e4cf82c
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-08 14:57:04 +0000

    more solved

--- lib/questions.rb
+++ lib/questions.rb
@@ -154,6 +154,8 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  Hash[array.each_slice(2).to_a]
+  #h = Hash[*a]
 end
 
 # get all the letters used in an array of words and return
@@ -161,6 +163,13 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  tab = []
+  array.each do |word|
+    word.each_char do |char|
+      tab << char if !tab.include?(char)
+    end
+  end
+  tab.sort
 end
 
 # swap the keys and values in a hash. e.g.
